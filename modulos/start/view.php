4b3f3b6d86f79229003cf8a7a909c4e308629af2
DavidtucDesarrollos <davidtuc@hotmail.com>
2019-09-26 15:46:27 -0300

    Cambio de Colores

--- modulos/start/view.php
+++ modulos/start/view.php
@@ -9,7 +9,7 @@
   </section>
   
   <!-- Main content -->
-  <section class="content" style="background:#CF0;">
+  <section class="content" style="background:#CFF;">
     <div class="row">
       <div class="col-lg-12 col-xs-12">
         <div class="alert alert-info alert-dismissable">
